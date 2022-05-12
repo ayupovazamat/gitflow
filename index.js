fbfbfebe864f896e0af552e019f094e1ff373433
ayupovazamat <ayupovazamat@gmail.com>
2022-05-13 00:49:57 +0500

    Добавляем кнопку чтоб чтоб открывалось окно

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert('gitflow')
+const btn = document.createElement("button");
+btn.innerHTML = "Git Flow";
+btn.className = 'btn';
+document.body.appendChild(btn);
+
+const btnElem = document.querySelector('btn');
+btnElem.addEventListener("click", function () {
+alert('git flow')
+});
